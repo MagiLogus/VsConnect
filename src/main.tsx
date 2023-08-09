9c7bff1f08cf27d6d74d1f9fc579e51b8d29032a
Paulo Gonçalves <paulo.oliveira.phgo@gmail.com>
2023-08-08 21:29:10 -0300

    atividade finalizada

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//componentes
+import Home from "./pages/Home/";
+import ListaServicos from './pages/ListaServicos';
+
+
+
+//estilização global
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+    <ListaServicos/>
+  </React.StrictMode>,
+)
